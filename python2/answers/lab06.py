f3a7927e13b7d200f9f6635266fda4c3a884bc78
Insequent <deathbeforedishes@gmail.com>
2015-03-04 21:30:19 +0000

    Making lab06 simplier and the answer more clear

--- python2/answers/lab06.py
+++ python2/answers/lab06.py
@@ -4,17 +4,16 @@
     Lab06 - Decorators
 """
 
-import inspect
 import random
 
-names = ["Susan","Carol","Bob","Jeanette","Larry","John"]
+names = ["BoB", "BOB", "Susan","Carol","Bob","Jeanette","Larry","John"]
 
 def bouncer(func):
     def kick_bob(attendees):
         print("Looking for Bob...")
-        if "Bob" in attendees:
-            print("Dropping Bob...")
-            attendees.remove("Bob")
+        for attendee in attendees:
+            if attendee.lower() == "bob":
+                attendees.remove(attendee)
         func(attendees)
     return kick_bob
 
@@ -22,11 +21,6 @@ def bouncer(func):
 def party(attendees):
     print("List of people going to the party: {}".format(", ".join(attendees)))
 
+print(party)
 party(names)
 
-print(party)
-print(bouncer)
-looksee = inspect.getmembers(bouncer)
-for x, y in looksee:
-    if type(y) is not dict:
-        print("{}: {}".format(x, y))
